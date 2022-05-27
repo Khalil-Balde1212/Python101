64cede97c7b24b50cc0a13ee21f508277c87e7d3
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2022-05-27 11:43:33 -0400

    Created session 2 example code
    Example of using if else statements and boolean data types

---
+++ Session2/Session2Example.py
@@ -0,0 +1,54 @@
+import random
+
+player_name = ""
+player_max_hp = 10;
+player_cur_hp = 10;
+player_weapon = 0;
+player_weapon_dmg = [1, 2, 4, 6, 100] 
+
+print("Hello welcome to Dino Hunter")
+print("What is your name?")
+player_name = input()
+
+print()
+print()
+print()
+print("Hello " + player_name + "! Are you ready to fight dinosaurs? (type y or n)")
+if(input() == "y"):
+    gameStart = True
+    print("Awesome let's get started")
+else:
+    gameStart = False
+    print("Tough. See you never I guess")
+
+#start the game
+if(gameStart == True):
+    print()
+    print()
+    print()
+    print("As you set foot on Dinoland, the humidity of the jungle before you warms you as you enter the jungle")
+    input("Press enter to continue...")
+    print("\n\n\n")
+    print("Walking into the jungle you see a waterfall. Next to it you see something rustling in the bushes")
+    print("What do you do? (Answer with the number)")
+    print(" 1. Investigate the waterfall")
+    print(" 2. Continue down")
+
+    choice = input()
+    if(choice == "1"):
+        print("You investigate the waterfall and you find a DEFINETLY JUST A NERF BLASTER AND NOTHING VIOLENT")
+        print("Your attack increases to 2")
+        input("Press enter to continue...")
+    elif(choice == "2"):
+        print("The waterfall is too dangerous so you continue forward")
+        input("Press enter to continue...")
+
+    #get attacked
+    print("\n\n\n")
+    print("As you continue through the foliage, you are suddenly attacked by a Compsagnathus!!!")
+    input("Press enter to continue...")
+    print("\n\n\n")
+    print("The rest of this game is under development!")
+
+    
+
