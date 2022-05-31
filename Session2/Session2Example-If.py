9901f6444bce231ef0a6a7220c312948f39f6027
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2022-05-31 10:45:49 -0400

    Create Session2Example-If.py

---
+++ Session2/Session2Example-If.py
@@ -0,0 +1,11 @@
+eggs = input("How many eggs are in the fridge? : ")
+eggs = int(eggs)
+
+if(eggs >= 48):
+    print("Gaston can become roughly")
+    print("the size of a barge")
+elif(eggs >= 12):
+    print("Gaston will eat these to get large")
+else:
+    print("Gaston will not get large")
+    print("Please buy more eggs :(")
