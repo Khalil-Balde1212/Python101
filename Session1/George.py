9f3d2a5ad371423880eea9c38eef351914c78c0f
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2022-05-31 10:33:10 -0400

    Uploaded Session 1 code
    Code created during Session 1

---
+++ Session1/George.py
@@ -0,0 +1,30 @@
+
+
+health = 10
+pi = 3.14159265358979
+
+print("Hello welcome to the world of pokemon. What is your name?")
+name = input()
+print()
+print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+message = "hello from the other siiiiiiiiiiide"
+
+print("hello world!")
+print("Live Laugh Love")
+print("Never Gonna Give you Up")
+print(message)
